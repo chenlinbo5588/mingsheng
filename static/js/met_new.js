fc877e8360699bb93975c524d08a7951ac7f3cd3
lichun.gao <lichungao.2007@163.com>
2014-05-17 11:09:51 +0800

    add temp hp

---
+++ static/js/met_new.js
@@ -0,0 +1,32 @@
+jq(function(){
+    
+    jq('.asktop2 li').bind("mouseenter",function(e){
+        jq(this).parents("ul").find("li").removeClass('askcur');
+        jq(this).addClass("askcur");
+        
+        var id=jq(this).attr('lang');
+		jq('.bmlogos').hide();
+		jq('.ask_'+id).show();
+    });
+    
+    jq('.asktop2 li').bind("mouseleave",function(e){
+        
+    });
+    
+    /**
+	jq('.asktop2 li').click(function(){
+		jq('.asktop2 li').removeClass('askcur');
+		jq(this).addClass('askcur');
+		
+	});
+    */
+});
+jq(function(){
+//	jq('#zxwd li').mouseover(function(){
+//		jq('#zxwd li').removeClass('askcur');
+//		jq(this).addClass('askcur');
+//		var id=jq(this).attr('lang');
+//		jq('.showno').hide();
+//		jq('.zxwd_'+id).show();
+//	});
+});
